b7b9328a315ef75cb366b4420ce91047bd4deed2
Hao He <hehao1998@pku.edu.cn>
2019-04-12 11:44:02 +0800

    Added Account Page

--- src/router.js
+++ src/router.js
@@ -31,7 +31,7 @@ export default new Router({
       component: () => import('./views/Project.vue')
     },
     {
-      path: '/account',
+      path: '/account/:accountId',
       name: 'account',
       component: () => import('./views/Account.vue')
     },
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="account">
-    <h1>This is an account page</h1>
+    <h1>This is an account profile page</h1>
+    <el-container>
+      <el-aside width="200px">
+        <h3>个人资料</h3>
+        {{accountName}}
+        <img :src="photoUrl" width="150px">
+        <el-button>关注</el-button>
+      </el-aside>
+      <el-main>
+        <el-tabs>
+          <el-tab-pane label="TA发布的" name="posts"></el-tab-pane>
+          <el-tab-pane label="正在关注" name="watching"></el-tab-pane>
+          <el-tab-pane label="关注TA的" name="watched"></el-tab-pane>
+        </el-tabs>
+      </el-main>
+    </el-container>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    accountId: 0,
+    accountName: 'admin',
+    photoUrl: '',
+    watching: [],
+    watchedBy: []
+  })
+}
+</script>
+
+<style>
+</style>
